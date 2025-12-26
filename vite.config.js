817cd74c7cb9edfaeca9df05aae7d2333e753d42
lightning4747 <vignesh112847@gmail.com>
2025-12-26 11:56:52 +0530

    depoly setup

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+base: "/Memory-card-game-using-react/",
+plugins: [react()],
+});
